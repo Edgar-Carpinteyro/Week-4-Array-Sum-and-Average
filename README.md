a0d55ce1510601d4a8173ab11b828f0a8a73c743
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2023-10-26 18:29:19 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Week-4-Array-Sum-and-Average
---
+++ index.js
@@ -0,0 +1,14 @@
+let array1 = [2, 4, 6, 8, 10];
+
+let sum;
+do {
+  sum = 0;
+  for (let i = 0; i < array1.length; i++) {
+    sum += array1[i];
+  }
+
+} while (array1.length[4]) 
+console.log(sum)
+
+let average = sum / array1.length
+console.log(average)
